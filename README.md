e87f4b72e58f5e8a4a671bd510151cc792cae91c
Yutaka Catch <yyycatch@gmail.com>
2021-08-13 21:35:19 +0900

    Add server and cgi

--- README.md
+++ README.md
@@ -1,3 +1,15 @@
 # RubyWikiMini
  
 This is small wiki-engine of Ruby.
+
+## Usage
+
+```
+$ ruby srv.rb
+```
+
+> index.htmlを表示する
+> http://127.0.0.1:8080/index.html
+
+> cgi.rbの結果を表示する
+> http://127.0.0.1:8080/cgi?a=hoge
---
+++ srv.rb
@@ -0,0 +1,15 @@
+# srv.rb
+# http://127.0.0.1:8080/index.html
+# http://127.0.0.1:8080/cgi?a=hoge
+
+require 'webrick'
+srv = WEBrick::HTTPServer.new({
+    :DocumentRoot => './',
+    :BindAddress => '127.0.0.1',
+    :Port => 8080,
+    :CGIInterpreter => 'C:\Ruby27-x64\bin\ruby.exe'     # for Windows
+})
+srv.mount('/cgi', WEBrick::HTTPServlet::CGIHandler, './cgi.rb')
+srv.mount('/foo.html', WEBrick::HTTPServlet::FileHandler, 'hoge.html')
+trap("INT"){ srv.shutdown }
+srv.start
